c5215fbb276f507ba1e4d74e405c0701806c8cad
ArchitShah248 <architshah248@gmail.com>
2016-08-28 11:49:12 +0530

    - Removed API key

--- app/src/main/java/com/portfolio/archit/weathermaster/data/AppConstants.java
+++ app/src/main/java/com/portfolio/archit/weathermaster/data/AppConstants.java
@@ -5,6 +5,9 @@ package com.portfolio.archit.weathermaster.data;
  */
 public final class AppConstants {
 
-    public static final String OPEN_WEATHER_MAP_API_KEY = "***REMOVED***";
+    public static final String YOUR_OPEN_WEATHER_API_KEY = "YOUR OPEN WEATHER API KEY";
+
+    //Enter your Open Weather API key here
+    public static final String OPEN_WEATHER_MAP_API_KEY = YOUR_OPEN_WEATHER_API_KEY;
 
 }
